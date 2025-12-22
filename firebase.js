cb3141b74ce9d62e2228c74207cc13e23f77c44a
akhilesh <akhilgarg8085@gmail.com>
2025-12-22 22:48:25 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,7 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getAuth,
-  GoogleAuthProvider,
-  signInWithRedirect,
-  getRedirectResult,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDciz4ZaVuf11LA_MXb7a3YpmWbM6VyRC4",
@@ -19,27 +13,6 @@ const firebaseConfig = {
   measurementId: "G-YSSVM003KJ"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const provider = new GoogleAuthProvider();
-
-// button click pe yeh call karo (mobile + desktop dono ke liye safe)
-export function loginWithGoogle() {
-  signInWithRedirect(auth, provider);
-}
-
-// page load hone par (index.js ya main script me) yeh code zaroor chalao:
-getRedirectResult(auth).then(async (result) => {
-  if (!result) return;
-  const user = result.user;
-  if (!user) return;
-
-  // yahin Firestore me user save karo, taaki mobile par bhi hamesha chale
-  await setDoc(doc(db, "users", user.uid), {
-    uid: user.uid,
-    name: user.displayName,
-    email: user.email,
-    photoURL: user.photoURL
-  });
-});
